9184997962ccb778ee0a8fb6eb81187965f46341
Augusto G. Borges <127455184+augustogborges@users.noreply.github.com>
2026-03-25 00:35:18 +0000

    exercicio-3

---
+++ sorveteria.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main () {
+
+    char tipo;
+    int quant;
+    
+    printf("Digite o sabor de sorvete: ");
+        scanf("%c", &tipo);
+
+    printf("Digite a quantidade de bolas: ");
+        scanf("%i", &quant);
+    
+    if (tipo == 'M' || tipo == 'm'){
+        printf("O valor da conta ficará %.2f e voce esta comprando sorvete de Morango" , quant*1.5);
+    } else if (tipo == 'C' || tipo == 'c') {
+        printf("O valor da conta ficará %.2f e voce esta comprando sorvete de Creme", quant*1.5);
+    } else if (tipo == 'F' || tipo == 'f') {
+        printf("O valor da conta ficará %.2f e voce esta comprando sorvete de Flocos" , quant*1.5);
+    } else if (tipo == 'A' || tipo == 'a'){
+        printf("O valor da conta ficará %.2f e voce esta comprando sorvete de Ameixa" , quant*1.5);
+    } else{
+        printf("Não existe esse sorvete ");
+    }
+}
+
